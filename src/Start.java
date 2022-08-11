d5c30e62e9ea7c9f02490da5a9d00acada5efb9b
nikingrid <nikpam2008@hotmail.com>
2022-08-11 14:18:17 +0200

    Inicio if

--- src/Start.java
+++ src/Start.java
@@ -4,10 +4,12 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for (int i=0; i<6; i++ ) {
-			System.out.println(i);
+		for (int i=0; i<21; i++ ) {
+			if (i==2) {
+				System.out.println(i);
+			}
+			
 		}
-		
 	}
 
 }
