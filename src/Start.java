524e3d5b7987645a21ecb70d47111385a06c18f6
nikingrid <nikpam2008@hotmail.com>
2022-08-11 14:33:48 +0200

    Posible solución larga

--- src/Start.java
+++ src/Start.java
@@ -4,8 +4,9 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+				
 		for (int i=0; i<21; i++ ) {
-			if (i==2) {
+			if (i!=3 && i!=5 && i!=7 && i!=9 && i!=11 && i!=13 && i!=17 && i!=15 && i!=19) {
 				System.out.println(i);
 			}
 			
