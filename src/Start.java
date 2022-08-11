bee040391286175dc8f78dc4745157ee75b01a82
nikingrid <nikpam2008@hotmail.com>
2022-08-11 18:26:43 +0200

    Add introducir un numero y te saca si es par o impar

--- src/Start.java
+++ src/Start.java
@@ -1,14 +1,23 @@
+import java.util.Scanner;
 
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner add=new Scanner(System.in);
 		
-		for (int i=0; i<=20; i++) {
-			if (i % 2 == 0) {
-				System.out.println(i);}		
-		}
+		int i;
+		
+		System.out.print("Introduce un número entero: ");
+		i=add.nextInt();
+		
+		if (i % 2 == 0) {
+				System.out.println("Par");
+				}
+			else {
+				System.out.print("Ímpar");
+			}
 		
 	}
 
